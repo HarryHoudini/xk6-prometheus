24e8db536a853b8f156df05849d2a745aeb68719
Harry Houdini <dev.jon.jonson@gmail.com>
2023-05-26 18:33:35 +0300

    draft: sd

--- internal/prometheus.go
+++ internal/prometheus.go
@@ -131,6 +131,7 @@ func (a *PrometheusAdapter) tagsToLabelValues(labelNames []string, sampleTags *m
 
 	if len(tags) > 0 {
 		// a.logger.WithField("unused_tags", tags).Warn("Not all tags used as labels")
+		a.logger.WithField("unused_tags", tags).Warn("")
 	}
 
 	a.logger.WithField("tags", tags).Info("__________________________tags____________________________")
